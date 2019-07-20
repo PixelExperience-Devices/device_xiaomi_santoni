2d4a94125e2ad4ec30b28275012f42708be0f5a0
Zainudin Shamilov <42618943+ErrorNetwork28@users.noreply.github.com>
2019-07-20 19:37:03 +0300

    Change defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ TARGET_USES_QTI_CAMERA_DEVICE := true
 TARGET_TS_MAKEUP := true
 
 # Kernel
-TARGET_KERNEL_CONFIG := santoni_nontreble_defconfig
+TARGET_KERNEL_CONFIG := santoni_defconfig
 
 # Init
 TARGET_INIT_VENDOR_LIB         := libinit_santoni
