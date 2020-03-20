98f6047d407f084660bb0d98be194211822ce996
Pranaya Deomani <deomanipranaya007@gmail.com>
2020-03-20 19:13:11 +0530

    Revert "santoni: Introduce Safailnet"
    This reverts commit 90d3463bb15aa36669fbcb85a36a8621bf092ad3.

--- device.mk
+++ device.mk
@@ -383,8 +383,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc \
-    init.safailnet.rc
+    ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
     init.baseband.sh \
